167eec1692080b3c3ccfd12c919d662c232f85ef
rizwanawanfb <rizwanawanfb@gmail.com>
2024-05-02 12:02:26 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let buttons = document.querySelectorAll('.button');
+// console.log(buttons)
+let body = document.querySelector("body");
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function (e) {
+        if (e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
